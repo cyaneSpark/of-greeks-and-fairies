8475e05c9403b87383c3b1d119b525ce977accf1
Vasileios Karavasilis <cyane.spark@gmail.com>
2021-11-20 22:25:55 +0200

    Started events for ingredient placement

--- Assets/Scripts/MainInteractionManager.cs
+++ Assets/Scripts/MainInteractionManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using Fairies;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using static DG.Tweening.DOTween;
 
 namespace Fairies
@@ -12,6 +13,48 @@ namespace Fairies
     {
         public GameObject hand_grandma, hand_doctor, hand_priest;
         private Dictionary<Actor, GameObject> hands = new Dictionary<Actor, GameObject>();
+        
+        public event EventHandler<IInteractionManager.DeliveryArgs> onTryToDeliver;
+
+        private void TryDeliver(Actor actor, Item item) =>
+            onTryToDeliver?.Invoke(this, new IInteractionManager.DeliveryArgs(actor, item));
+        
+        public void Test(SelectEnterEventArgs args)
+        {
+            ItemMono item = args.interactable.GetComponent<ItemMono>();
+
+            Actor actor = Actor.doctor;
+
+            if (args.interactor.CompareTag("Doc"))
+            {
+                actor = Actor.doctor;
+
+            }
+            else if (args.interactor.CompareTag("Granny"))
+            {
+                actor = Actor.grandma;
+            }
+            else if(args.interactor.CompareTag("Priest"))
+            {
+                actor = Actor.priest;
+            }
+            else
+            {
+                Debug.LogError("WHO IS SENDING THIS? INVALID ACTOR TAG!", args.interactor);
+                return;
+            }
+            
+            if (item != null)
+            {
+                
+                Debug.Log("SOMEONE THREW" + item.ID + "IN ME!");
+            }
+            else
+            {
+                Debug.Log("SOMEONE THREW NOTHING IN ME!");
+            }
+        }
+
 
         private void Awake()
         {
@@ -29,20 +72,22 @@ namespace Fairies
             {
                 case IInteractionManager.ActorMotion.Appear:
                     hands[actor].SetActive(true);
-                    hands[actor].transform.DOLocalMoveZ(-0.4f, 1);
+                    hands[actor].transform.DOLocalMoveX(-0.4f, 0.8f).SetEase(Ease.InOutElastic);
                     break;
                 case IInteractionManager.ActorMotion.Reject:
                     LogInfo("ANIMATE {0} REJECT", actor);
                     break;
                 case IInteractionManager.ActorMotion.Disappear:
-                    LogInfo("ANIMATE {0} DISAPPEAR", actor);
-                    hands[actor].SetActive(false);
+                    hands[actor].transform.DOLocalMoveX(-0.8f, 0.8f).SetEase(Ease.OutExpo).OnComplete(() =>
+                    {
+                        hands[actor].SetActive(false);
+                    });
                     break;
                 default:
                     break;
             }
         }
 
-        public event EventHandler<IInteractionManager.DeliveryArgs> onTryToDeliver;
+        
     }
 }
