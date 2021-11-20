3bd9741377dc0a9141466c29bd6494273841d3c0
Vasileios Karavasilis <cyane.spark@gmail.com>
2021-11-20 21:52:23 +0200

    Imported DOTween
    Made appear animation

--- Assets/Scripts/MainInteractionManager.cs
+++ Assets/Scripts/MainInteractionManager.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Fairies;
 using UnityEngine;
+using static DG.Tweening.DOTween;
 
 namespace Fairies
 {
@@ -10,7 +12,7 @@ namespace Fairies
     {
         public GameObject hand_grandma, hand_doctor, hand_priest;
         private Dictionary<Actor, GameObject> hands = new Dictionary<Actor, GameObject>();
-        
+
         private void Awake()
         {
             hands.Add(Actor.grandma, hand_grandma);
@@ -20,14 +22,14 @@ namespace Fairies
             foreach (GameObject gO in hands.Values)
                 gO.SetActive(false);
         }
-        
+
         public void HandleActorMotion(Actor actor, IInteractionManager.ActorMotion motion)
         {
             switch (motion)
             {
                 case IInteractionManager.ActorMotion.Appear:
-                    LogInfo("ANIMATE {0} APPEAR", actor);
                     hands[actor].SetActive(true);
+                    hands[actor].transform.DOLocalMoveZ(-0.4f, 1);
                     break;
                 case IInteractionManager.ActorMotion.Reject:
                     LogInfo("ANIMATE {0} REJECT", actor);
